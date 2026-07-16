f55da44d82c7a21fb82e6041bcc7d8ec0b30ab0e
어비 유누스 <abiyunus@eobis-MacBook-Pro.local>
2026-07-16 14:27:28 +0900

    style.css Update

--- update.sh
+++ update.sh
@@ -3,3 +3,15 @@
 git add .
 git commit -m "Auto Update"
 git push
+
+git add .
+git commit -m "style.css Update"
+git push
+
+git add .
+git commit -m "index.html Update"
+git push
+
+git add .
+git commit -m "script.js Update"
+git push
